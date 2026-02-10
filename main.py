a01a13eda6b8032507b660128ced0bd387e61225
Rafael <ryalico83@gmail.com>
2026-02-09 23:36:27 -0500

    Add logging to debug create_license 500 error

--- main.py
+++ main.py
@@ -296,7 +296,7 @@ async def root():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run(
-        "server:app",
+        "main:app",
         host="0.0.0.0",
         port=port,
         reload=False,
